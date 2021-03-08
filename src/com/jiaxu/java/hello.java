9e2d8e62fdfabe1c61cf57582974dda621aef1c7
wjx002 <wjx3059180244@gmail.com>
2021-03-08 19:33:54 +0800

    第一次尝试用idea提交到github

---
+++ src/com/jiaxu/java/hello.java
@@ -0,0 +1,20 @@
+package com.jiaxu.java;
+
+/**
+ * @Author：吴家旭
+ * @date：2021/3/7 - 17:51
+ */
+public class hello {
+    public static void main(String[] args) {
+        int i = 0;
+        int k;
+
+
+        int j;
+
+
+
+
+
+    }
+}
