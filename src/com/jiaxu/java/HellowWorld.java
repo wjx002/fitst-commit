7217a86aa5aec3ff4e377e6af2bf702bacbe3853
wjx002 <wjx3059180244@gmail.com>
2021-03-08 20:29:04 +0800

    DebugTest试一下再一次push上github

---
+++ src/com/jiaxu/java/HellowWorld.java
@@ -0,0 +1,12 @@
+package com.jiaxu.java;
+
+public class HellowWorld {
+    //最强大的world
+    /**
+     * @Author:吴家旭
+     *
+     * */
+    public static void  main(String[] args){
+        System.out.println("Helloworld!!");
+    }
+}
